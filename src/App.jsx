88ebcd2b20bc2d21c629883d8f5d037c429ab889
aceptio <budakinung12@gmail.com>
2026-04-18 03:18:51 +0700

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -91,7 +91,7 @@ function App() {
             />
             <div className="flex items-center sm:gap-4 gap-2">
               <a  
-              href="https://drive.google.com/file/d/1yj35tHVC-NeTOLFdARZbWn_UgPK-jNUz/view?usp=drive_link"
+              href="https://drive.google.com/file/d/1kedUK_nTC7ZaK_j0Zd_RQz_aQJahrGtX/view?usp=sharing"
               download="AcepTio_CV.pdf"
               target="_blank"
               rel="noopener noreferrer"
